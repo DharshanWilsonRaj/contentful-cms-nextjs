192c6835dda2e76a5fb2d2b5f8efbda4698d2d82
Dharshan <dharshan@selvisoftware.com>
2024-11-09 11:40:49 +0530

    error message fixed

--- config/contentful.js
+++ config/contentful.js
@@ -1,8 +1,8 @@
 
 import * as contentful from "contentful"
 const client = contentful.createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_API_TOKEN,
+    space: process.env.CONTENTFUL_SPACE_ID || 'ruv608cqwfxn',
+    accessToken: process.env.CONTENTFUL_API_TOKEN || 'WFGudubKnl21xBw72EpN_zeaiFqhj5uYiq_cBgTI7_k',
     environment: process.env.CONTENTFUL_ENVIRONMENT_ID || 'master', // Optional, defaults to 'master'
 
 })
