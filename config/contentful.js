83b941fb1385b661e3de583265b289d1d0b2d9bc
Dharshan <dharshan@selvisoftware.com>
2024-11-09 11:30:09 +0530

    content ful

--- config/contentful.js
+++ config/contentful.js
@@ -3,5 +3,7 @@ import * as contentful from "contentful"
 const client = contentful.createClient({
     space: process.env.CONTENTFUL_SPACE_ID,
     accessToken: process.env.CONTENTFUL_API_TOKEN,
+    environment: process.env.CONTENTFUL_ENVIRONMENT_ID || 'master', // Optional, defaults to 'master'
+
 })
 export default client
